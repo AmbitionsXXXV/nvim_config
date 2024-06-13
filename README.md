fdf13b4d9bea1be41d11523aa58dbf6676d12efc
etcetera <yilonglei790@gmail.com>
2024-06-13 20:47:10 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,13 @@
+# 关于本项目
+
+fork from [这个仓库](https://github.com/nshen/InsisVim)
+
+详细配置请参考原仓库的 README.md
+
+个人修改了一些配置，增加了一些插件，主要是为了方便自己的使用
+
+比如使用 biome、ruff 分别作为 ts、python 的 linter、formatter
+
+## 首页字体
+
+[配置地址](https://patorjk.com/software/taag/)
---
+++ init.lua
@@ -0,0 +1,29 @@
+require("insis").setup({
+  cmp = {
+    copilot = true,
+  },
+  frontend = {
+    enable = true,
+    tailwindcss = true,
+    format_on_save = true,
+  },
+  python = {
+    enable = true,
+    formatter = "ruff",
+    format_on_save = true,
+  },
+  rust = {
+    enable = true,
+    formatter = "rustfmt",
+    format_on_save = true,
+  },
+  toml = {
+    enable = true,
+    formatter = "rustfmt",
+    format_on_save = true,
+  },
+  golang = {
+    enable = true,
+    format_on_save = true,
+  },
+})
